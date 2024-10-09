e176f5d7c7ee129fb2c0f357e6290daaab1d6b2c
adon1as <adonias.dev@gmail.com>
2024-10-09 14:16:43 -0300

    variaveis de ambiente para o front

--- front/covid19dashbord/src/app/api.service.ts
+++ front/covid19dashbord/src/app/api.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private apiUrl = 'http://localhost:8081/countryVaccinatio';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
